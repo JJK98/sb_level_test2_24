b7a5a46cab62500f27feb2b456da02bd81efce77
JJk <wnsrb1594@naver.com>
2024-09-25 11:56:28 +0900

    2-8 자기글만 삭제,수ì정가능

--- src/main/java/com/board/basic/article/ArticleController.java
+++ src/main/java/com/board/basic/article/ArticleController.java
@@ -1,13 +1,22 @@
 package com.board.basic.article;
 
+import com.board.basic.user.SiteUser;
+import com.board.basic.user.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.security.Principal;
 import java.util.List;
 
 @Controller
@@ -16,6 +25,7 @@ import java.util.List;
 public class ArticleController {
 
     private final ArticleService articleService;
+    private final UserService userService;
 
     @GetMapping("/list")
     public String list(Model model) {
@@ -24,14 +34,21 @@ public class ArticleController {
         return "article_list";
     }
 
+    @PreAuthorize("isAuthenticated()")
     @GetMapping("/create")
     public String create(ArticleForm articleForm) {
         return "article_form";
     }
 
+    @PreAuthorize("isAuthenticated()")
     @PostMapping("/create")
-    public String create(@RequestParam(value = "title") String title, @RequestParam(value = "content") String content) {
-        this.articleService.create(title, content);
+    public String create(@Valid ArticleForm articleForm,
+                         BindingResult bindingResult, Principal principal) {
+        if (bindingResult.hasErrors()) {
+            return "article_form";
+        }
+        SiteUser siteUser = this.userService.getUser(principal.getName());
+        this.articleService.create(articleForm.getTitle(), articleForm.getContent(), siteUser);
         return "redirect:/article/list";
     }
 
@@ -42,9 +59,13 @@ public class ArticleController {
         return "article_detail";
     }
 
+    @PreAuthorize("isAuthenticated()")
     @GetMapping("/modify/{id}")
-    public String articleModify(ArticleForm articleForm, @PathVariable("id")Integer id){
+    public String articleModify(ArticleForm articleForm, @PathVariable("id")Integer id, Principal principal){
         Article article = this.articleService.getArticle(id);
+        if(!article.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
+        }
         articleForm.setTitle(article.getTitle());
         articleForm.setContent(article.getContent());
         return "article_form";
